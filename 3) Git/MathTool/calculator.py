d11ddd1c9d28c4e3a8f96fd1158a2a12afc4698b
joungeun <lje0818ovo@naver.com>
2020-10-06 10:26:06 +0900

    Add divide function

--- 3) Git/MathTool/calculator.py
+++ 3) Git/MathTool/calculator.py
@@ -5,10 +5,8 @@ def add(a, b):
 def subtract(a,b):
 	return a-b
 
-<<<<<<< HEAD:3) Git/MathTool/calculator.py
 def divide(a, b):
     return a/b
-=======
+    
 def multiply(a,b);
 	return a*b
->>>>>>> 4c29ca09a0763dafdaa9abda42c716dc61db3ba4:3) Git/calculator.py
